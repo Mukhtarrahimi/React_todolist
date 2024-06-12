6641740b625697a905a023e7664b25dd4476b034
Mukhtarrahimi <mukhtarrahimi110@gmail.com>
2024-06-13 00:56:29 +0430

    add task code

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Todoitems from "./Todoitems";
 
 const Todo = () => {
+  const [todoList, setTodoList] = useState([]);
+  const inputRef = useRef();
+  const addTask = () => {
+    const inputText = inputRef.current.value.trim(); // .trim() remove the extra whitespace from start and end
+    if (inputText === "") {
+      return null;
+    }
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isComplate: false,
+    };
+    setTodoList((prev) => [...prev, newTodo]);
+    inputRef.current.value = "";
+  };
   return (
     <div className="bg-white place-self-center  w-11/12 max-w-md flex flex-col p-10 rounded-xl">
       {/*-------------- title  -------------*/}
@@ -16,12 +31,14 @@ const Todo = () => {
 "
         >
           <input
+            ref={inputRef}
             id="input"
             type="text"
             className="h-full ps-1 text-sm w-full border focus:border-blue-200 rounded outline-none bg-transparent"
             placeholder="Add your task"
           />
           <label
+            onClick={addTask}
             htmlFor="input"
             className="bg-blue-400 cursor-pointer hover:bg-blue-500 duration-300 px-6 rounded-md h-full text-center text-white"
           >
@@ -30,7 +47,16 @@ const Todo = () => {
         </div>
         {/*-----------------  input box list  -----------*/}
         <div className="mt-6 flex flex-col gap-4">
-          <Todoitems />
+          {todoList.map((list) => {
+            return (
+              <Todoitems
+                key={list.id}
+                id={list.id}
+                text={list.text}
+                isComplate={list.isComplate}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
--- src/components/Todoitems.jsx
+++ src/components/Todoitems.jsx
@@ -3,12 +3,12 @@ import tick from "../assets/tick.png";
 import not_tick from "../assets/not_tick.png";
 import delete_icon from "../assets/delete.png";
 
-const Todoitems = () => {
+const Todoitems = ({text}) => {
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-4 cursor-pointer">
         <img src={tick} className="w-8" alt="" />
-        <p>learn coding</p>
+        <p>{text}</p>
       </div>
       <img src={delete_icon} className="w-4 cursor-pointer" alt="" />
     </div>

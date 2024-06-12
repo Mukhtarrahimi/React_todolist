eb7d0955144eccf0fdddd8b1fe08c1d36f5ff18c
Mukhtarrahimi <mukhtarrahimi110@gmail.com>
2024-06-13 00:25:22 +0430

    title and input code

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Todo from "./components/Todo";
 
 const App = () => {
   return (
-    <div>
+    <div className="bg-gradient-to-r from-sky-400 to-blue-500 w-full min-h-screen grid ">
       <Todo />
     </div>
   );
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const Todo = () => {
   return (
-    <div>
-      <h1>ToDo</h1>
-    </div>
-  )
-}
+    <div className="bg-white place-self-center  w-11/12 max-w-md flex flex-col p-10 rounded-xl">
+      {/*-------------- title  -------------*/}
+      <div>
+        <h1 className="text-center font-semibold text-blue-500 text-3xl font-Rubik">
+          to-do list
+        </h1>
+        {/*----------------- input box  -----------*/}
+        <div
+          className="input bg-white border border-blue-400 flex gap-1 items-center justify-between w-full h-10 rounded-md p-1 mt-6 shadow-inner;
 
-export default Todo
+"
+        >
+          <input
+            id="input"
+            type="text"
+            className="h-full ps-1 text-sm w-full border focus:border-blue-200 rounded outline-none bg-transparent"
+            placeholder="Add your task"
+          />
+          <label
+            htmlFor="input"
+            className="bg-blue-400 cursor-pointer hover:bg-blue-500 duration-300 px-6 rounded-md h-full text-center text-white"
+          >
+            ADD
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
